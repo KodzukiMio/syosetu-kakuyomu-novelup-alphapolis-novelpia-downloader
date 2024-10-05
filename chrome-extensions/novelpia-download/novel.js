59f274ccc4bbc731b4e89cd92d36c20a89131a04
KodzukiMio <87706365+KodzukiMio@users.noreply.github.com>
2024-10-05 13:24:29 +0800

    Delete chrome-extensions/novelpia-download/novel.js

--- chrome-extensions/novelpia-download/novel.js
+++
@@ -1,5 +0,0 @@
-var __novelpia_dl = __novelpia_dl || {};
-(async () => {
-    __novelpia_dl.init(__novelpia_dl.NOVEL);
-    __novelpia_dl.set_listener(__novelpia_dl.STATE, () => __novelpia_dl.handle());
-})();
